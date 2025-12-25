89b2fec1b4c2f05f95682207ed42a20474a1a689
Shell Reaper <76930483+shellreaper@users.noreply.github.com>
2025-12-25 16:01:09 +0000

    Update auto_recon.sh

--- auto_recon.sh
+++ auto_recon.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#add bruteforcing using puredns later
+#add sublist3r later
+#addd dnsgen later
 
 # Function to display beautiful screens
 display_screen() {
@@ -49,7 +52,7 @@ command -v assetfinder >/dev/null 2>&1 || { echo >&2 "Assetfinder is not install
 command -v naabu >/dev/null 2>&1 || { echo >&2 "Naabu is not installed. Aborting."; exit 1; }
 command -v httpx >/dev/null 2>&1 || { echo >&2 "Httpx is not installed. Aborting."; exit 1; }
 command -v nuclei >/dev/null 2>&1 || { echo >&2 "Nuclei is not installed. Aborting."; exit 1; }
-command -v dnsx >/dev/null 2>&1 || { echo >&2 "Dnsx is not installed. Aborting."; exit 1; }
+command -v dnsgen >/dev/null 2>&1 || { echo >&2 "Dnsx is not installed. Aborting."; exit 1; }
 
 # Check if domain and output folder provided
 if [ $# -ne 2 ]; then
@@ -78,40 +81,34 @@ if [ "$skip" = false ]; then
 
     # Running subdomain enumeration with other tools
     echo "[*] Running subdomain enumeration with amass and subfinder..."
-    amass enum -d "$DOMAIN" -o "$OUTPUT_FOLDER/amass.txt" -passive -v
-    # Filtering Amass results to include only plain subdomains
-    awk -F'|' '{print $2}' "$OUTPUT_FOLDER/amass.txt" | grep -Eo '[a-zA-Z0-9._-]+\.'"$DOMAIN" > "$OUTPUT_FOLDER/amass_filtered.txt"
-    subfinder -d "$DOMAIN" -o "$OUTPUT_FOLDER/subfinder.txt"
+    amass enum -active -passive -d "$DOMAIN" 
+    amass subs -names -nocolor -d "$DOMAIN" > "$OUTPUT_FOLDER/amass.txt"
+    # Finding subdomains using subfinder
+    subfinder -all -d "$DOMAIN" -o "$OUTPUT_FOLDER/subfinder.txt"
 
     # Perform DNS bruteforcing with dnsx and Jhaddix wordlist on the root domain
-    echo "[*] Performing DNS bruteforcing with dnsx and Jhaddix wordlist on the root domain..."
-   # dnsx -d "$DOMAIN" -w /usr/share/seclists/Discovery/DNS/dns-Jhaddix.txt -o "$OUTPUT_FOLDER/dnsx_bruteforce.txt" -r /usr/share/seclists/Miscellaneous/dns-resolvers.txt
+    echo "[*] Performing DNS bruteforcing with Puredns and Jhaddix wordlist on the root domain..."
+    puredns bruteforce /usr/share/SecLists/Discovery/DNS/dns-Jhaddix.txt "$DOMAIN" -w "$OUTPUT_FOLDER/puredns.txt" -l 2000
 
-    # Combine dnsx results with existing subdomains
+
+    # Combine purends results with existing subdomains
     echo "[*] Combining dnsx results with existing subdomains..."
-    cat "$OUTPUT_FOLDER/dnsx_bruteforce.txt" >> "$OUTPUT_FOLDER/amass_filtered.txt"
-    cat "$OUTPUT_FOLDER/amass_filtered.txt" >> "$OUTPUT_FOLDER/subfinder.txt"
+    cat "$OUTPUT_FOLDER/puredns.txt" >> "$OUTPUT_FOLDER/amass.txt"
+    cat "$OUTPUT_FOLDER/amass.txt" >> "$OUTPUT_FOLDER/subfinder.txt"
     cat "$OUTPUT_FOLDER/subfinder.txt" >> "$OUTPUT_FOLDER/assetfinder.txt"
 
     # Filter unique subdomains
     echo "[*] Filtering unique subdomains..."
     sort -u "$OUTPUT_FOLDER/assetfinder.txt" -o "$OUTPUT_FOLDER/subdomains.txt"
-fi
+    #Permutating Subdomains 
+    echo "[*] Subdomain Permutation ..."
+    cat "$OUTPUT_FOLDER/subdomains.txt" | dnsgen - | sort -u | puredns resolve -w "$OUTPUT_FOLDER/dnsgen.txt" -l 5000
+    cat  "$OUTPUT_FOLDER/subdomains.txt" "$OUTPUT_FOLDER/dnsgen.txt"| sort -u > "$OUTPUT_FOLDER/allsubs.txt"
 
+fi
+   
 # Reset Ctrl+C flag
-skip=false
 
-# Phase 2: Port Scanning
-if [ "$skip" = false ]; then
-    display_screen "Port Scanning"
-    echo "[*] Running Naabu for port scanning..."
-
-    # Create a directory to save Naabu output
-    mkdir -p "$OUTPUT_FOLDER/naabu"
-
-    # Run Naabu port scanning
-    naabu -l "$OUTPUT_FOLDER/subdomains.txt" -o "$OUTPUT_FOLDER/naabu/naabu.txt" --rate 10000 -p 0-65535 -s s
-fi
 
 # Reset Ctrl+C flag
 skip=false
@@ -120,7 +117,9 @@ skip=false
 if [ "$skip" = false ]; then
     display_screen "HTTP Probing"
     echo "[*] Running Httpx for HTTP probing..."
-    cat "$OUTPUT_FOLDER/subdomains.txt" | httpx -retries 2 -title -tech-detect -follow-redirects -o "$OUTPUT_FOLDER/httpx.txt"
+    cat "$OUTPUT_FOLDER/allsubs.txt" | httpx -retries 2 -title -tech-detect -follow-redirects -o "$OUTPUT_FOLDER/httpx.txt"
+    cat "$OUTPUT_FOLDER/allsubs.txt" | httpx  -o "$OUTPUT_FOLDER/urls_clean.txt"
+    gowitness scan file -f "$OUTPUT_FOLDER/urls_clean.txt" -s "$OUTPUT_FOLDER/screenshots"
 fi
 
 # Reset Ctrl+C flag
@@ -130,7 +129,24 @@ skip=false
 if [ "$skip" = false ]; then
     display_screen "Vulnerability Scanning"
     echo "[*] Running Nuclei for vulnerability scanning..."
-    nuclei -l "$OUTPUT_FOLDER/subdomains.txt" -t ~/nuclei-templates/ -o "$OUTPUT_FOLDER/nuclei_results.txt" -es info
+    nuclei -l "$OUTPUT_FOLDER/allsubs.txt" -t ~/nuclei-templates/ -o "$OUTPUT_FOLDER/nuclei_results.txt" -es info
+fi
+curl -s -X POST "https://api.telegram.org/bot{{API_KEY}}/sendMessage" \
+-d "chat_id={{CHAT_ID}}" \
+-d "text=$(printf 'Your Scan is Finished :\n"%s"' "$(cat $OUTPUT_FOLDER/nuclei_results.txt | grep -v 'ssl')")"
+
+skip=false
+
+ Phase 2: Port Scanning
+if [ "$skip" = false ]; then
+    display_screen "Port Scanning"
+    echo "[*] Running Naabu for port scanning..."
+
+    #Create a directory to save Naabu output
+    mkdir -p "$OUTPUT_FOLDER/naabu"
+
+    #Run Naabu port scanning
+    sudo naabu -l "$OUTPUT_FOLDER/allsubs.txt" -o "$OUTPUT_FOLDER/naabu/naabu.txt" -verify --rate 10000 -p 0-65535 -s s
 fi
 sudo chown -R `whoami`:`whoami` $OUTPUT_FOLDER/
 echo -e "\e[1;32m[*] Recon phase completed.\e[0m"
